935cb72dc40fd56cd21a45dd3e04c74a475e8178
XYmar <xqingliuwu@163.com>
2019-04-11 16:19:45 +0800

    查找二次修改申请后的文件

--- src/main/java/com/rengu/cosimulation/repository/SublibraryFilesAuditRepository.java
+++ src/main/java/com/rengu/cosimulation/repository/SublibraryFilesAuditRepository.java
@@ -6,11 +6,13 @@ import com.rengu.cosimulation.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 @Repository
 public interface SublibraryFilesAuditRepository extends JpaRepository<SublibraryFilesAuditEntity, String> {
     boolean existsBySublibraryFilesEntityAndUserEntityAndStateAndIfOver(SublibraryFilesEntity sublibraryFilesEntity, UserEntity userEntity, int state, boolean ifOver);
+    List<SublibraryFilesAuditEntity> findBySublibraryFilesEntityAndCreateTimeAfter(SublibraryFilesEntity sublibraryFilesEntity, Date sublibraryDate);
     List<SublibraryFilesAuditEntity> findByUserEntityAndState(UserEntity userEntity, int state);
     List<SublibraryFilesAuditEntity> findBySublibraryFilesEntity(SublibraryFilesEntity sublibraryFilesEntity);
     List<SublibraryFilesAuditEntity> findBySublibraryFilesEntityAndIfOver(SublibraryFilesEntity sublibraryFilesEntity, boolean ifOver);
